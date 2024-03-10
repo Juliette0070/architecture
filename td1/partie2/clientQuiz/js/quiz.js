aa0cc9d3dc15a3e4c5972e2a45f1d79c5f9c0f99
Juliette0070 <juliette.rousselet@etu.univ-orleans.fr>
2024-03-10 23:32:41 +0100

    ajout suppression/creation/modification question et quiz

--- td1/partie2/clientQuiz/js/quiz.js
+++ td1/partie2/clientQuiz/js/quiz.js
@@ -20,7 +20,9 @@ $(function() {
 
     function remplirQuiz(repjson) {
       $('#liste-quiz').empty();
-      $('#liste-quiz').append($('<h2>').text("Liste des quiz"));
+      $('#liste-quiz')
+        .append($('<h2>').text("Liste des quiz"))
+        .append($('<input type="button" value="add quiz">').on("click", formNewQuiz));
       $('#liste-quiz').append($('<ul>'));
       for(questionnaire of repjson.questionnaires){
           $('#liste-quiz ul')
@@ -32,8 +34,14 @@ $(function() {
         }
     }
 
+    function formNewQuiz(){
+        formQuiz();
+        $("#currentquiz #liste-questions").append($('<input type="button" value="save quiz">').on("click", saveNewQuiz));
+        $("#currentquiz #liste-questions").children("input[value='modify quiz']").remove();
+        $("#currentquiz #liste-questions").children("input[value='delete quiz']").remove();
+    }
+
     function detailsQuiz(event){
-        $("#currentquiz").empty();
         formQuiz();
         fillFormQuiz(event.data);
     }
@@ -47,12 +55,89 @@ $(function() {
             .append($('<span>Questionnaire: <input type="text" id="questionnaire"><br></span>'))
             .append("<ul id='questions'></ul>")
             .append($('<input type="hidden" id="turi">'))
-            .append($('<input type="button" value="modify quiz">'))
-            .append($('<input type="button" value="delete quiz">'));
+            .append($('<input type="button" value="add question">').on("click", addQuestion))
+            .append($('<input type="button" value="modify quiz">').on("click", saveModifiedQuiz))
+            .append($('<input type="button" value="delete quiz">').on("click", delQuiz));
         $("#currentquiz #question")
             .append($('<span>Question: <input type="text" id="questionSelectionnee"><br></span>'))
-            .append($('<input type="button" value="modify question">'))
-            .append($('<input type="button" value="delete question">'));
+            .append($('<hidden id="quri">'))
+            .append($('<div id="type">'))
+            .append($('<div>')
+                .append($('<input type="button" value="simplequestion">').on("click", simpleSelected))
+                .append($('<input type="button" value="multiplequestion">').on("click", multipleSelected))
+                .append($('<input type="button" value="autre">').on("click", autreSelected))
+            )
+            .append($('<input type="button" value="save question">').on("click", saveNewQuestion));
+    }
+
+    function addQuestion(){
+        $("#currentquiz #question #type").empty();
+        $("#currentquiz #question #questionSelectionnee").val("");
+        $("#currentquiz #question input[value='simplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='multiplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='autre']").css("color", "blue");
+    }
+
+    function simpleSelected(){
+        $("#currentquiz #question #type")
+            .empty()
+            .append($('<span>Reponse: <input type="text" id="reponse"><br></span>'));
+        $("#currentquiz #question input[value='simplequestion']").css("color", "blue");
+        $("#currentquiz #question input[value='multiplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='autre']").css("color", "black");
+    }
+
+    function multipleSelected(){
+        $("#currentquiz #question #type")
+            .empty()
+            .append($('<span>Reponse: <input type="text" id="reponse"><br></span>'))
+            .append($('<span>Choix 1: <input type="text" id="choix1"><br></span>'))
+            .append($('<span>Choix 2: <input type="text" id="choix2"><br></span>'));
+        $("#currentquiz #question input[value='simplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='multiplequestion']").css("color", "blue");
+        $("#currentquiz #question input[value='autre']").css("color", "black");
+    }
+
+    function autreSelected(){
+        $("#currentquiz #question #type").empty();
+        $("#currentquiz #question input[value='simplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='multiplequestion']").css("color", "black");
+        $("#currentquiz #question input[value='autre']").css("color", "blue");
+    }
+
+    function saveNewQuestion(){
+        if ($("#currentquiz #question #type").children().length == 1){
+            var question = new SimpleQuestion(
+                $("#currentquiz #questionSelectionnee").val(),
+                $("#currentquiz #reponse").val()
+            );
+        } else if ($("#currentquiz #question #type").children().length == 3){
+            var question = new MultipleQuestion(
+                $("#currentquiz #questionSelectionnee").val(),
+                $("#currentquiz #reponse").val(),
+                $("#currentquiz #choix1").val(),
+                $("#currentquiz #choix2").val()
+            );
+        } else {
+            var question = new Question(
+                $("#currentquiz #questionSelectionnee").val()
+            );
+        }
+        console.log(JSON.stringify(question));
+        uri = $("#currentquiz #turi").val();
+        fetch(uri + "/questions",{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(question)
+        })
+        .then(res => {console.log('Save Success') ;
+            $("#result").text(res['contenu']);
+            refreshQuizList();
+        })
+        .catch( res => { console.log(res) });
     }
 
     function fillFormQuiz(t){
@@ -90,19 +175,31 @@ $(function() {
     function remplirQuestion(repjson) {
         $("#currentquiz #question").empty();
         $("#currentquiz #question")
-            .append($('<span>Question: <input type="text" id="questionSelectionnee" value="' + repjson.title + '"><br></span>'));
+            .append($('<span>Question: <input type="text" id="questionSelectionnee" value="' + repjson.title + '"><br></span>'))
+            .append($('<div id="type">'));
+        $("#currentquiz #question")
+            .append($('<div>')
+                .append($('<input type="button" value="simplequestion">').on("click", simpleSelected))
+                .append($('<input type="button" value="multiplequestion">').on("click", multipleSelected))
+                .append($('<input type="button" value="autre">').on("click", autreSelected))
+            )
+            .append($('<input type="button" value="modify question">').on("click", saveModifiedQuestion))
+            .append($('<input type="button" value="delete question">').on("click", delQuestion));
+        $("#currentquiz #question")
+            .append($('<input type="hidden" id="quri" value="' + repjson.uri + '">'));
         if (repjson.questionnaireType == "simplequestion"){
-            $("#currentquiz #question")
+            $("#currentquiz #question #type")
                 .append($('<span>Reponse: <input type="text" id="reponse" value="' + repjson.reponse + '"><br></span>'));
+            $("#currentquiz #question input[value='simplequestion']").css("color", "blue");
         } else if (repjson.questionnaireType == "multiplequestion"){
-            $("#currentquiz #question")
-            .append($('<span>Reponse: <input type="text" id="reponse" value="' + repjson.reponse + '"><br></span>'))
+            $("#currentquiz #question #type")
+                .append($('<span>Reponse: <input type="text" id="reponse" value="' + repjson.reponse + '"><br></span>'))
                 .append($('<span>Choix 1: <input type="text" id="choix1" value="' + repjson.choix1 + '"><br></span>'))
                 .append($('<span>Choix 2: <input type="text" id="choix2" value="' + repjson.choix2 + '"><br></span>'));
+            $("#currentquiz #question input[value='multiplequestion']").css("color", "blue");
+        } else {
+            $("#currentquiz #question input[value='autre']").css("color", "blue");
         }
-        $("#currentquiz #question")
-            .append($('<input type="button" value="modify question">'))
-            .append($('<input type="button" value="delete question">'));
     }
 
     function onerror(err) {
@@ -120,8 +217,8 @@ $(function() {
     }
 
     class Quiz{
-        constructor(title, uri, questions){
-            this.title = title;
+        constructor(name, uri, questions){
+            this.name = name;
             this.uri = uri;
             this.questions = questions;
         }
@@ -131,12 +228,14 @@ $(function() {
         constructor(title, uri){
             this.title = title;
             this.uri = uri;
+            this.questionnaireType = "question";
         }
     }
 
     class SimpleQuestion extends Question{
         constructor(title, uri, reponse){
             super(title, uri);
+            this.questionnaireType = "simplequestion";
             this.reponse = reponse;
         }
     }
@@ -144,6 +243,7 @@ $(function() {
     class MultipleQuestion extends Question{
         constructor(title, uri, reponse, choix1, choix2){
             super(title, uri);
+            this.questionnaireType = "multiplequestion";
             this.reponse = reponse;
             this.choix1 = choix1;
             this.choix2 = choix2;
@@ -151,17 +251,8 @@ $(function() {
     }
 
     function saveNewQuiz(){
-        //var questions = [];
-        //for (var i = 0; i < $("#currentquiz #questions").children().length; i++){
-        //    var question = new Question(
-        //        $("#currentquiz #questions").children().eq(i).children().eq(1).val(),
-        //        $("#currentquiz #questions").children().eq(i).children().eq(0).val()
-        //    );
-        //    questions.push(question);
-        //}
         var quiz = new Quiz(
-            $("#currentquiz #questionnaire").val(),
-            []
+            $("#currentquiz #questionnaire").val()
         );
         console.log(JSON.stringify(quiz));
         fetch("http://localhost:5000/quiz/api/v1.0/quiz",{
@@ -179,45 +270,21 @@ $(function() {
         .catch( res => { console.log(res) });
     }
 
-    function saveNewQuiz1(){
-        var task = new Task(
-            $("#currentquiz #titre").val(),
-            $("#currentquiz #descr").val(),
-            $("#currentquiz #done").is(':checked')
-            );
-        console.log(JSON.stringify(task));
-        fetch("http://localhost:5000/todo/api/v1.0/tasks",{
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(task)
-            })
-        .then(res => { console.log('Save Success') ;
-                       $("#result").text(res['contenu']);
-                       refreshQuizList();
-                   })
-        .catch( res => { console.log(res) });
-    }
-
     function saveModifiedQuiz(){
-        var task = new Task(
-            $("#currentquiz #titre").val(),
-            $("#currentquiz #descr").val(),
-            $("#currentquiz #done").is(':checked'),
+        var quiz = new Quiz(
+            $("#currentquiz #questionnaire").val(),
             $("#currentquiz #turi").val()
-            );
+        );
         console.log("PUT");
-        console.log(task.uri);
-        console.log(JSON.stringify(task));
-        fetch(task.uri,{
+        console.log(quiz.uri);
+        console.log(JSON.stringify(quiz));
+        fetch(quiz.uri,{
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         method: "PUT",
-        body: JSON.stringify(task)
+        body: JSON.stringify(quiz)
             })
         .then(res => { console.log('Update Success');  refreshQuizList();} )
         .catch( res => { console.log(res) });
@@ -228,14 +295,65 @@ $(function() {
         url = $("#currentquiz #turi").val();
         fetch(url,{
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         },
         method: "DELETE"
             })
         .then(res => { console.log('Delete Success:' + res); } )
         .then(refreshQuizList)
         .catch( res => { console.log(res);  });
+        }
     }
-  }
+
+    function saveModifiedQuestion(){
+        if ($("#currentquiz #question #type").children().length == 1){
+            var question = new SimpleQuestion(
+                $("#currentquiz #questionSelectionnee").val(),
+                $("#currentquiz #reponse").val()
+            );
+        } else if ($("#currentquiz #question #type").children().length == 3){
+            var question = new MultipleQuestion(
+                $("#currentquiz #questionSelectionnee").val(),
+                $("#currentquiz #reponse").val(),
+                $("#currentquiz #choix1").val(),
+                $("#currentquiz #choix2").val()
+            );
+        } else {
+            var question = new Question(
+                $("#currentquiz #questionSelectionnee").val()
+            );
+        }
+        console.log(JSON.stringify(question));
+        uri = $("#currentquiz #turi").val();
+        fetch(uri,{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "PUT",
+            body: JSON.stringify(question)
+        })
+        .then(res => {console.log('Save Success') ;
+            $("#result").text(res['contenu']);
+            refreshQuizList();
+        })
+        .catch( res => { console.log(res) });
+    }
+
+    function delQuestion(){
+        if ($("#currentquiz #questionSelectionnee").val()){
+            url = $("#currentquiz #quri").val();
+            fetch(url,{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "DELETE"
+                })
+            .then(res => { console.log('Delete Success:' + res); } )
+            .then(refreshQuizList)
+            .catch( res => { console.log(res);  });
+        }
+    }
 });
