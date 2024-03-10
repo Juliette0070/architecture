61152724827181ad2a837473ed93cce3037e6ab0
Juliette0070 <juliette.rousselet@etu.univ-orleans.fr>
2024-03-10 04:51:37 +0100

    ajoute bouttons pur la suppression (non fonctionnels) et fix affichage + affichage des reponses/choix pour les questions qui en ont

--- td1/partie2/clientQuiz/js/quiz.js
+++ td1/partie2/clientQuiz/js/quiz.js
@@ -2,23 +2,8 @@ $(function() {
 
     $("#button").click(refreshQuizList);
 
-    function remplirQuiz(repjson) {
-      $('#liste-quiz').empty();
-      $('#liste-quiz').append($('<h2>').text("Liste des quiz"));
-      $('#liste-quiz').append($('<ul>'));
-      for(questionnaire of repjson.questionnaires){
-          $('#liste-quiz ul')
-                .append($('<li>')
-                .append($('<a>')
-                .text(questionnaire.name)
-                    ).on("click", questionnaire, detailsQuiz)
-                );
-        }
-      }
-
-      function onerror(err) {
-          $("#quiz").html("<b>Impossible de récupérer les quiz à réaliser !</b>"+err);
-      }
+    $("#tools #add").on("click", formQuiz);
+    $('#tools #del').on('click', delQuiz);
 
     function refreshQuizList(){
         $("#currentquiz").empty();
@@ -31,19 +16,69 @@ $(function() {
             )
         .then(remplirQuiz)
         .catch(onerror);
-      }
+    }
 
+    function remplirQuiz(repjson) {
+      $('#liste-quiz').empty();
+      $('#liste-quiz').append($('<h2>').text("Liste des quiz"));
+      $('#liste-quiz').append($('<ul>'));
+      for(questionnaire of repjson.questionnaires){
+          $('#liste-quiz ul')
+                .append($('<li>')
+                .append($('<a>')
+                .text(questionnaire.name)
+                    ).on("click", questionnaire, detailsQuiz)
+            );
+        }
+    }
 
     function detailsQuiz(event){
         $("#currentquiz").empty();
         formQuiz();
         fillFormQuiz(event.data);
     }
+
+    function formQuiz(){
+        $("#currentquiz").empty();
+        $("#currentquiz")
+            .append($('<div id="liste-questions">'))
+            .append($('<div id="question">'))
+        $("#currentquiz #liste-questions")
+            .append($('<span>Questionnaire: <input type="text" id="questionnaire"><br></span>'))
+            .append("<ul id='questions'></ul>")
+            .append($('<input type="hidden" id="turi">'))
+            .append($('<input type="button" value="modify quiz">'))
+            .append($('<input type="button" value="delete quiz">'));
+        $("#currentquiz #question")
+            .append($('<span>Question: <input type="text" id="questionSelectionnee"><br></span>'))
+            .append($('<input type="button" value="modify question">'))
+            .append($('<input type="button" value="delete question">'));
+    }
+
+    function fillFormQuiz(t){
+        $("#currentquiz #questionnaire").val(t.name);
+        $("#currentquiz #turi").val(t.uri);
+        for (var i = 0; i < t.questions.length; i++){
+            fetch(t.questions[i])
+            .then(response => {
+                if (response.ok) return response.json();
+                else throw new Error('Problème ajax: '+response.status);
+            })
+            .then(response => {
+                $("#currentquiz #questions")
+                .append($('<li>')
+                .append($('<hidden>').val(response.uri))
+                .append($('<a id="'+response.uri+'">')
+                .text(response.title))
+                .on("click", (function(question) {return function() {detailsQuestion(question);};})(response.uri))
+                )
+            })
+        }
+    }
     
     function detailsQuestion(uri){
-        requete = uri;
-        fetch(requete)
-        .then( response => {
+        fetch(uri)
+        .then(response => {
                   if (response.ok) return response.json();
                   else throw new Error('Problème ajax: '+response.status);
                 }
@@ -55,10 +90,24 @@ $(function() {
     function remplirQuestion(repjson) {
         $("#currentquiz #question").empty();
         $("#currentquiz #question")
-            .append($('<input type="text" id="questionSelectionnee" value="' + repjson.title + '"><br>'))
-            .append($('<input type="button" value="modify question">'));
+            .append($('<span>Question: <input type="text" id="questionSelectionnee" value="' + repjson.title + '"><br></span>'));
+        if (repjson.questionnaireType == "simplequestion"){
+            $("#currentquiz #question")
+                .append($('<span>Reponse: <input type="text" id="reponse" value="' + repjson.reponse + '"><br></span>'));
+        } else if (repjson.questionnaireType == "multiplequestion"){
+            $("#currentquiz #question")
+            .append($('<span>Reponse: <input type="text" id="reponse" value="' + repjson.reponse + '"><br></span>'))
+                .append($('<span>Choix 1: <input type="text" id="choix1" value="' + repjson.choix1 + '"><br></span>'))
+                .append($('<span>Choix 2: <input type="text" id="choix2" value="' + repjson.choix2 + '"><br></span>'));
+        }
+        $("#currentquiz #question")
+            .append($('<input type="button" value="modify question">'))
+            .append($('<input type="button" value="delete question">'));
     }
 
+    function onerror(err) {
+        $("#quiz").html("<b>Impossible de récupérer les quiz à réaliser !</b>"+err);
+    }
 
     class Task{
         constructor(title, description, done, uri){
@@ -71,50 +120,66 @@ $(function() {
     }
 
     class Quiz{
-        constructor(title){
+        constructor(title, uri, questions){
             this.title = title;
+            this.uri = uri;
+            this.questions = questions;
         }
     }
 
-
-    $("#tools #add").on("click", formQuiz);
-    $('#tools #del').on('click', delQuiz);
-
-    function formQuiz(isnew){
-        $("#currentquiz").empty();
-        $("#currentquiz")
-            .append($('<div id="liste-questions">'))
-            .append($('<div id="question">'))
-        $("#currentquiz #liste-questions")
-            .append($('<span>Questionnaire: <input type="text" id="questionnaire"><br></span>'))
-            .append("<ul id='questions'></ul>")
-            .append($('<input type="hidden" id="turi">'))
-            .append($('<input type="button" value="modify quiz">'));
-        $("#currentquiz #question")
-            .append($('<span>Question<input type="text" id="question"><br></span>'))
-            .append($('<input type="button" value="modify question">'));
+    class Question{
+        constructor(title, uri){
+            this.title = title;
+            this.uri = uri;
         }
+    }
 
-    function fillFormQuiz(t){
-        $("#currentquiz #questionnaire").val(t.name);
-        for (var i = 0; i < t.questions.length; i++){
-            $("#currentquiz #questions")
-            .append($('<li>')
-            .append($('<a>')
-            .text(t.questions[i]))
-            .on("click", (function(question) {return function() {detailsQuestion(question);};})(t.questions[i]))
-            );
+    class SimpleQuestion extends Question{
+        constructor(title, uri, reponse){
+            super(title, uri);
+            this.reponse = reponse;
         }
+    }
 
-        $("#currentquiz #titre").val(t.title);
-        $("#currentquiz #descr").val(t.description);
-         t.uri=(t.uri == undefined)?"http://localhost:5000/todo/api/v1.0/tasks"+t.id:t.uri;
-         $("#currentquiz #turi").val(t.uri);
-        t.done?$("#currentquiz #done").prop('checked', true):
-        $("#currentquiz #done").prop('checked', false);
+    class MultipleQuestion extends Question{
+        constructor(title, uri, reponse, choix1, choix2){
+            super(title, uri);
+            this.reponse = reponse;
+            this.choix1 = choix1;
+            this.choix2 = choix2;
+        }
     }
 
     function saveNewQuiz(){
+        //var questions = [];
+        //for (var i = 0; i < $("#currentquiz #questions").children().length; i++){
+        //    var question = new Question(
+        //        $("#currentquiz #questions").children().eq(i).children().eq(1).val(),
+        //        $("#currentquiz #questions").children().eq(i).children().eq(0).val()
+        //    );
+        //    questions.push(question);
+        //}
+        var quiz = new Quiz(
+            $("#currentquiz #questionnaire").val(),
+            []
+        );
+        console.log(JSON.stringify(quiz));
+        fetch("http://localhost:5000/quiz/api/v1.0/quiz",{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(quiz)
+        })
+        .then(res => {console.log('Save Success') ;
+            $("#result").text(res['contenu']);
+            refreshQuizList();
+        })
+        .catch( res => { console.log(res) });
+    }
+
+    function saveNewQuiz1(){
         var task = new Task(
             $("#currentquiz #titre").val(),
             $("#currentquiz #descr").val(),
